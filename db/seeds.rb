55a9cc00ff3119d06ac2c8d6ed63e3e5b04499ff
Andrew Nashkolniy <andrew.nashkolniy@gmail.com>
2023-08-19 15:05:22 +0300

    Add seed samples

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
 # frozen_string_literal: true
+
+require 'csv'
+
+SAMPLE_HEADER = 'INFO'
+
+CSV.foreach(Rails.root.join('db', 'datasets', 'sample.csv'), headers: true) do |row|
+  PromptQuery.create!(query: row[SAMPLE_HEADER])
+end
